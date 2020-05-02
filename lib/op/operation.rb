52a8b1654443322edeb05e51030b609e4f50e05f
Alex Vasyutin <alex.vasyutin@gmail.com>
2020-05-02 18:13:22 +0300

    Add workaround for failure when we cannot correctly dump ActionController::Parameters as JSON

--- lib/op/operation.rb
+++ lib/op/operation.rb
@@ -64,10 +64,34 @@ module Op
       @state = OperationState.create!(
         name: operation_name,
         context: context.as_json,
-        args: args,
+        args: safe_args(args),
         emitter_type: context.emitter_type,
         emitter_id: context.emitter_id
       )
+    end
+
+    # There is convention to not pass complex data types in args but sometimes when we need to
+    # handle ActionController::Parameters it may contain ActionDispatch::Http::UploadedFile which is
+    # not always can be correctly serialized into JSON. Sometimes it raises Encoding::UndefinedConversionError
+    # because of special characters in headers field. If this happens we catch error and just dump
+    # entire args as string to debug purpose.
+    # This is example of broken args:
+    # => {"image"=>
+    #   #<ActionDispatch::Http::UploadedFile:0x0000564a0dfa0b60
+    #    @content_type="image/jpeg",
+    #    @headers=
+    #     "Content-Disposition: form-data; name=\"website_image[image]\"; filename=\"httplab \xD0\xBB\xD0\xBE\xD0\xB3\xD0\xBE\xD1\x82\xD0\xB8\xD0\xBF.jpg\"\r\nContent-Type: image/jpeg\r\n",
+    #    @original_filename="httplab логотип.jpg",
+    #    @tempfile=#<File:/tmp/RackMultipart20200502-18311-ra5vtv.jpg>>}
+    def safe_args(args)
+      # Try to convert args to JSON to emit Encoding::UndefinedConversionError
+      args.to_json
+
+      # return args if there is no error
+      args.as_json
+    rescue Encoding::UndefinedConversionError
+      # Otherwise return special hash with args dumped into string
+      { '_' => args.inspect }
     end
 
     def perform_steps(*args)
